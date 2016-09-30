74a8633d6ff95c8fd18eb53bc8737a0acfe70374
Ilay Raz <ilay.raz@gmail.com>
2016-09-30 12:05:55 -0700

    1-20

--- 1-20.c
+++ 1-20.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
+#define TABSPACE 4
 #define MAXLINE 1000
 
 int getsline (char line[], int maxline);
-void reverse (char line[], int length);
+void detab (char line[], int length);
 
 int main (void) {
     int len;
     char line[MAXLINE];
 
     while ((len = getsline(line, MAXLINE)) > 0) {
-        reverse(line, len);
+        detab(line, len);
         printf("=>%s\n", line);
     }
 
@@ -25,23 +26,25 @@ int getsline(char s[], int lim) {
     if (c == '\n')
         s[i++] = c;
     s[i--] = '\0';
-
-    printf("getchar(%d): %s\n", i,s);
-
     return i;
 }
 
-void reverse (char s[], int len) {
-    int i;
-    char temp;
+void detab (char s[], int len) {
+    int i, n;
 
     while (s[len] == '\n' || s[len] == '\0')
         len--;
 
-    for (i = 0; i < (int)(len / 2) + 1; i++) {
-        temp = s[len - i];
-        s[len - i] = s[i];
-        s[i] = temp;
+    len++;
+    for (i = 0; i < len; i++) {
+        if (s[i] == '\t') {
+            for (n = len - 1; n > i; n--)
+                s[n+TABSPACE-1] = s[n];
+            for (n = 0; n < TABSPACE; n++)
+                s[n+i] = ' ';
+            len += TABSPACE;
+            i += TABSPACE - 1;
+        }
     }
     s[len+1] = '\0';
 }
